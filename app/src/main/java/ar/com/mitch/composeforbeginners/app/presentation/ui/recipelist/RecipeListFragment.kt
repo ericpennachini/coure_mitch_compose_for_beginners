0750160fdfd3a19a5ff2140c4ed926c33e62a614
Eric Pennachini <eric.pennachini@gmail.com>
2021-10-14 00:50:48 -0300

    added DefaultSnackbar component

--- app/src/main/java/ar/com/mitch/composeforbeginners/app/presentation/ui/recipelist/RecipeListFragment.kt
+++ app/src/main/java/ar/com/mitch/composeforbeginners/app/presentation/ui/recipelist/RecipeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -15,6 +16,8 @@ import androidx.compose.material.icons.twotone.Animation
 import androidx.compose.material.icons.twotone.CarRental
 import androidx.compose.material.icons.twotone.House
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -22,13 +25,18 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import ar.com.mitch.composeforbeginners.app.MainApplication
 import ar.com.mitch.composeforbeginners.app.presentation.components.*
 import ar.com.mitch.composeforbeginners.app.presentation.theme.AppTheme
+import ar.com.mitch.composeforbeginners.app.util.SnackBarController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @ExperimentalComposeUiApi
@@ -37,6 +45,8 @@ class RecipeListFragment : Fragment() {
 
     @Inject
     lateinit var application: MainApplication
+
+    private val snackBarController = SnackBarController(lifecycleScope)
 
     private val viewModel: RecipeListViewModel by viewModels()
 
@@ -53,6 +63,7 @@ class RecipeListFragment : Fragment() {
                     val recipes = viewModel.recipes.value
                     val query = viewModel.query.value
                     val selectedCategory = viewModel.selectedCategory.value
+                    val scaffoldState = rememberScaffoldState()
 
                     Scaffold(
                         topBar = {
@@ -68,9 +79,12 @@ class RecipeListFragment : Fragment() {
                                     application.toggleTheme()
                                 }
                             )
+                        },
+                        scaffoldState = scaffoldState,
+                        snackbarHost = {
+                            scaffoldState.snackbarHostState
                         }
                     ) {
-                        // all children fill the same space, it's not a stack
                         Box(
                             modifier = Modifier
                                 .fillMaxSize()
@@ -88,12 +102,27 @@ class RecipeListFragment : Fragment() {
                                     ) { _, recipe ->
                                         RecipeCard(
                                             recipe = recipe,
-                                            onClick = { }
+                                            onClick = {
+                                                snackBarController.getScope().launch {
+                                                    snackBarController.showSnackBar(
+                                                        scaffoldState = scaffoldState,
+                                                        message = recipe.title ?: "<EMPTY>",
+                                                        actionLabel = "Hide"
+                                                    )
+                                                }
+                                            }
                                         )
                                     }
                                 }
                             }
                             CircularIndeterminateProgressBar(isDisplayed = isLoading)
+                            DefaultSnackBar(
+                                snackBarHostState = scaffoldState.snackbarHostState,
+                                onDismiss = {
+                                    scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+                                },
+                                modifier = Modifier.align(Alignment.BottomCenter)
+                            )
                         }
                     }
                 }
